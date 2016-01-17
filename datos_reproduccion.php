bb7d7fcae9a7f50e28cb8fa27d588c22025f3b76
bffg1993 <bffg1993@gmail.com>
2016-01-16 20:45:30 -0500

    Vista reproducción Datos reproducción

---
+++ datos_reproduccion.php
@@ -0,0 +1,138 @@
+<div class="col-md-12">
+    <div class="col-md-offset-4 col-md-4">
+        <div class="col-md-6">
+            <p><strong>Nombre</strong></p>
+            <p><strong>Código</strong></p>
+        </div>
+        <div class="col-md-6">
+            <p>Josefa</p>
+            <p>1234</p>
+        </div>
+    </div>
+    <div class="col-md-4">
+        <p style="visibility: hidden">a</p>
+    </div>
+</div>
+<div class="col-md-12">
+    <table class="table-bordered">
+        <tr style="text-align: center">
+            <td class="col-md-4" colspan="3">
+                <strong>Programación</strong>
+            </td>
+            <td class="col-md-4" colspan="4">
+                <strong>INSEMINACIÓN O MONTA NATURAL</strong>
+            </td>
+            <td class="col-md-4" colspan="3">
+                <strong>Vaca N°</strong>
+            </td>
+        </tr>
+        <tr style="text-align: center">
+            <td class="col-md-1" rowspan="2">
+                
+            </td>
+            <td class="col-md-1" rowspan="2">
+                I.A.
+            </td>
+            <td class="col-md-1" rowspan="2">
+                M.N.
+            </td>
+            <td class="col-md-2">
+                1 I.A.
+            </td>
+            <td class="col-md-2">
+                2 I.A.
+            </td>
+            <td class="col-md-1">
+                3 I.A.
+            </td>
+            <td class="col-md-1" rowspan="2">
+                M.N.
+            </td>
+            <td class="col-md-3" colspan="3">
+                Palpaciones
+            </td>
+        </tr>
+        <tr>           
+            <td class="col-md-2">
+                
+            </td>
+            <td class="col-md-2">
+                
+            </td>
+            <td class="col-md-1">
+                
+            </td>
+            <td class="col-md-1" style="text-align: center">
+                1<sup>a</sup>
+            </td>
+            <td class="col-md-1" style="text-align: center">
+                2<sup>a</sup>
+            </td>
+            <td class="col-md-1" style="text-align: center">
+                3<sup>a</sup>
+            </td>
+        </tr>
+        <tr>
+            <td class="col-md-1">
+                F
+            </td>
+            <td class="col-md-1" colspan="2">
+                
+            </td>
+            <td class="col-md-2">
+                
+            </td>
+            <td class="col-md-2">
+                
+            </td>
+            <td class="col-md-1">
+                
+            </td>
+            <td class="col-md-1">
+                
+            </td>
+            <td class="col-md-1">
+                F
+            </td>
+            <td class="col-md-1">
+                F
+            </td>
+            <td class="col-md-1">
+                F
+            </td>
+        </tr>
+        <tr>
+            <td class="col-md-1">
+                T
+            </td>
+            <td class="col-md-1">
+                
+            </td>
+            <td class="col-md-1">
+                
+            </td>
+            <td class="col-md-2">
+                
+            </td>
+            <td class="col-md-2">
+                
+            </td>
+            <td class="col-md-1">
+                
+            </td>
+            <td class="col-md-1">
+                
+            </td>
+            <td class="col-md-1">
+                R
+            </td>
+            <td class="col-md-1">
+                R
+            </td>
+            <td class="col-md-1">
+                R
+            </td>
+        </tr>
+    </table>
+    
+</div>
---
+++ vista_reproduccion.php
@@ -0,0 +1,44 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" type="text/css" href="./css/estilos_generales.css">
+        <link rel="stylesheet" type="text/css" href="./css/bootstrap/css/bootstrap.min.css">
+        <script type="text/javascript" src="./css/bootstrap/js/jquery-2.1.4.min.js"></script>
+        <script type="text/javascript" src="./css/bootstrap/js/bootstrap.min.js"></script>        
+        <title>Vista reproducción</title>
+    </head>
+    <body>
+        <?php
+        include_once './head_vista_usuario.php';
+        ?>
+        <div class="container" style="padding-top: 70px">
+             
+                <h2>Sistema para la administración del ganado</h2>
+            
+            <ul class="nav nav-tabs">
+                <li role="presentation"><a href="./vista_usuario_1.php">Inicio</a></li>
+                <li role="presentation"><a href="?general">Datos de reproducción</a></li>
+            </ul>
+            <?php
+            $seccion = basename($_SERVER['QUERY_STRING']);
+            if (empty($seccion)) {
+                include 'datos_reproduccion.php';
+            } else {
+                $imp = 0;
+                switch ($seccion) {
+                    case 'general':
+                        $imp = 1;
+                        include 'datos_reproduccion.php';
+                        break;
+                }
+                if ($imp == 0) {
+                    echo $seccion + (': No existe esta sección');
+                }
+            }
+            ?> 
+        </div>
+
+        <?php
+        ?>
+    </body>
+</html>
