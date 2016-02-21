09e0a0a7783559deda911408e49db6c81d23daf9
bffg1993 <bffg1993@gmail.com>
2016-02-21 11:52:12 -0500

    Vista animales Add busqueda option

--- vista_animales.php
+++ vista_animales.php
@@ -25,8 +25,22 @@ include 'controlador/seguridadUsuarioAdmin.php';
         ?>
         <div class="container" style="padding-top: 70px">
 
-            <h2>Sistema para la administración del ganado</h2>
-            <input type="hidden" name="vaca" id="vaca" value="<?php echo $_SESSION['vaca'] ?>">
+            <div class="col-sm-12">
+                <div class="col-sm-7">
+                    <h2>Sistema para la administración del ganado</h2>
+                    <input type="hidden" name="vaca" id="vaca" value="<?php echo $_SESSION['vaca'] ?>">
+                </div>
+                <div class="col-sm-5" style="margin-top: 2em">
+                    <form class="form-inline">
+                        <div class="form-group">
+                            <label for="vacas">Código animal</label>
+                            <input type="text" class="form-control" id="vacas" placeholder="Ver otro animal">
+                        </div>                        
+                        <button type="submit" class="btn btn-default">Buscar</button>
+                    </form>
+                </div>
+            </div>
+
 
             <ul class="nav nav-tabs">
                 <li role="presentation"><a href="./vista_usuario_1.php">Inicio</a></li>
