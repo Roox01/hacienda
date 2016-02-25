f1ff68543cc40930214dbcb45149c16223fa8d22
pinformatica <pinformatica@ocastro.CeramicaItalia.com>
2016-02-25 17:57:11 -0500

    Falta inventario, base de datos concluida

--- vista_inventario.php
+++ vista_inventario.php
@@ -4,6 +4,9 @@ To change this license header, choose License Headers in Project Properties.
 To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
+<?php  
+    include 'controlador/seguridadUsuarioAdmin.php';
+?> 
 <html>
     <head>
         <meta charset="UTF-8">
@@ -21,7 +24,7 @@ and open the template in the editor.
         <div class="container" style="padding-top: 70px">
 
             <h2>Sistema para la administración del ganado</h2>
-            <input type="hidden" name="vaca" id="vaca" value="<?php echo$_POST['codigoVaca']; ?>">
+            <input type="hidden" name="hacienda" id="hacienda" value="<?php echo$_POST['hacienda']; ?>">
 
             <ul class="nav nav-tabs">
                 <li role="presentation"><a href="./vista_usuario_1.php">Inicio</a></li>
